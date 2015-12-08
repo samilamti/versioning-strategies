b058b5e6e3c18fb0592410c37c04eaa4e5fa173b
Sami Lamti <sami.lamti@if.se>
2015-12-08 16:26:51 +0100

    Added Service and Operation contracts to IProvideSytemInformation, referencing System.ServiceModel. Version bump: 1.2.0

--- MessageContracts/SystemInformation/IProvideSystemInformation.cs
+++ MessageContracts/SystemInformation/IProvideSystemInformation.cs
@@ -1,7 +1,11 @@
+using System.ServiceModel;
+
 namespace MessageContracts.SystemInformation
 {
+    [ServiceContract(Name = "SystemInformationService", Namespace = "http://waypoint.ifint.biz/systemInformation")]
     public interface IProvideSystemInformation
     {
+        [OperationContract]
         Response GetSystemInformation(Request informationRequest);
     }
 }
