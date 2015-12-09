1a7a5b6268cc41d3089ef84d60d0f70225dea44a
Sami Lamti <sami.lamti@if.se>
2015-12-09 08:10:54 +0100

    Added NServiceBus messaging to Producer

--- Producer/Program.cs
+++ Producer/Program.cs
@@ -5,6 +5,8 @@ using System.ServiceModel;
 using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
+using MessageContracts.SystemInformation;
+using NServiceBus;
 
 namespace Producer
 {
@@ -13,19 +15,44 @@ namespace Producer
         static void Main(string[] args)
         {
             var baseAddress = new Uri("http://localhost:8080/");
+            var busConfiguration = new BusConfiguration();
+            busConfiguration.EndpointName("non-breaking-changes.Producer");
+            busConfiguration.UseSerialization<JsonSerializer>();
+            busConfiguration.EnableInstallers();
+            busConfiguration.UsePersistence<InMemoryPersistence>();
+            busConfiguration.Conventions().DefiningEventsAs(t =>
+                t.Namespace.Contains("Contracts") &&
+                t.IsInterface &&
+                t.Name.StartsWith("I") && t.Name.EndsWith("ed"));
+
             using (var host = new ServiceHost(typeof(SystemInformationService), baseAddress))
+            using (var bus = Bus.Create(busConfiguration).Start())
             {
                 var smb = new ServiceMetadataBehavior
                 {
                     HttpGetEnabled = true,
-                    MetadataExporter = {PolicyVersion = PolicyVersion.Policy15}
+                    MetadataExporter = { PolicyVersion = PolicyVersion.Policy15 }
                 };
                 host.Description.Behaviors.Add(smb);
                 host.Open();
 
                 Console.WriteLine("The service is ready at {0}", baseAddress);
-                Console.WriteLine("Press <Enter> to stop the service.");
-                Console.ReadLine();
+                Console.WriteLine("Enter 'msg' to send a domain event.");
+                Console.WriteLine("Enter 'quit' to stop the service.");
+
+
+                var command = "";
+                while ((command = Console.ReadLine()) != "quit")
+                {
+                    if (command == "msg")
+                    {
+                        var serviceInstance = new SystemInformationService();
+                        var systemInformation = serviceInstance.GetSystemInformation(new Request {IncludeDriveInformation = true});
+                        bus.Publish<IDriveFreeSpaceChanged>(info => info.FreeSpace = systemInformation.FreeSpace.GetValueOrDefault(0));
+                        continue;
+                    }
+                    Console.WriteLine("Unknown command; try 'quit' or 'msg'");
+                }
 
                 host.Close();
             }
