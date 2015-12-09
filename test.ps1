7ccb5dbf8e2f0f6f59877a6b481b005c4b8946cc
Sami Lamti <sami.lamti@if.se>
2015-12-09 08:43:10 +0100

    We can now individually start consumer and producer

--- test.ps1
+++ test.ps1
@@ -1,2 +1,7 @@
-start .\Producer\bin\Debug\Producer.exe
-start .\Consumer\bin\Debug\Consumer.exe
+Param($what)
+if ($what -eq "producer" -or $what -eq "all") {
+	start .\Producer\bin\Debug\Producer.exe
+}
+if ($what -eq "consumer" -or $what -eq "all") {
+	start .\Consumer\bin\Debug\Consumer.exe
+}
