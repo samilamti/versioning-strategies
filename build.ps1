ae71dac01dc048cd0bc88fb9bfa009f89cdd6b6b
Sami Lamti <sami.lamti@if.se>
2016-02-09 13:10:55 +0100

    Updated build script to include nuget pack and restore

--- build.ps1
+++ build.ps1
@@ -1,3 +1,9 @@
 msbuild .\MessageContracts\MessageContracts.sln
+if (!(test-path -path lib)) {
+	mkdir lib
+}
+.\nuget.exe pack .\MessageContracts\MessageContracts.csproj -OutputDirectory lib
+.\nuget.exe restore .\Producer\Producer.sln
 msbuild .\Producer\Producer.sln
+.\nuget.exe restore .\Consumer\Consumer.sln
 msbuild .\Consumer\Consumer.sln
